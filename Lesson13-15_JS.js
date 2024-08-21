780869aa2a4ad8e94697402bd2e22fce2943aaee
Legenda00701 <deldla917@gmail.com>
2024-08-21 21:31:57 +0600

    -

---
+++ Lesson13-15_JS.js
@@ -0,0 +1,66 @@
+// 1
+let num = 42;
+console.log(num);
+
+// 2
+let str = "Hello, world!";
+console.log(str);
+
+// 3
+let isTrue = true;
+console.log(isTrue);
+
+// 4
+let a = 5;
+let b = 3;
+console.log(a+b);
+
+// 5
+const pi = 3.14;
+console.log(pi);
+
+// 6
+let name = "Emran";
+console.log(name);
+
+// 7
+let age = 20;
+console.log(age);
+
+// 8
+let messenge = "Welcome to my website";
+console.log(messenge);
+
+// 9
+let x = 10;
+let y = 5;
+console.log(x-y);
+
+// 10
+let firstName = "Эмран";
+let lastName = "Гусейнов";
+console.log(lastName + " " + firstName);
+
+// 11
+let width = 12;
+let heigth = 6;
+console.log("Площадь: " + width * heigth);
+
+//12
+let greeting = "Hello";
+//обращаюсь к уже созданному name
+console.log(greeting, name)
+
+//13
+let radius = 5;
+console.log("Площадь круга: " + radius * radius);
+
+//14
+let num1 = 10;
+let num2 = 3;
+console.log("Частное: " + num1 / num2);
+
+//15
+let num3 = 7;
+let num4 = 2;
+console.log("Остаток: " + num3 % num4)
